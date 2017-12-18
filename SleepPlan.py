3a41aa2c10318d03ef475bf3ffbd7ff68288b7ce
SynchronizedSynecdoche <kelly237@purdue.edu>
2017-12-18 17:06:52 -0500

    started testing around

--- SleepPlan.py
+++ SleepPlan.py
@@ -1,1 +1,17 @@
-print("I knew of the plan long before I knew she was even real, before any of us did")
+#have these be parameters passed in the CLI but that's a nightmare in python
+debugBool = True
+
+def debug(arg):
+    if(debugBool):
+        print("[DEBUG] {}".format(arg))
+
+
+startTime = input("When will you go to bed?\n>>>")
+
+startArr = startTime.split(":")
+startHour = startArr[0]
+startMin = startArr[1]
+
+debug("startHour: {} , startMin: {}".format(startHour,startMin))
+
+
